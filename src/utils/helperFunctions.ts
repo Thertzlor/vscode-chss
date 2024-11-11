74bdb1c323822d6be9205ec72d552d54d07ea5eb
Thertzlor <timo.hertzler@gmail.com>
2024-11-11 17:49:51 +0100

    we are caching range conversions now. Does it help performance? who knows.

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,9 +1,13 @@
 import {Range} from 'vscode';
 import type {Pseudo} from './chssParser';
 export type RangeIdentifier = ReturnType<typeof rangeToIdentifier>;
-export const rangeToIdentifier = ({start,end}:Range,pseudo?:Pseudo) => `${start.line}|${start.character}|${end.line}|${end.character}${pseudo?`|${pseudo}` as const:'' as const}` as const;
 
-export const identifierToRange = (ident:RangeIdentifier) => (([sl,sc,el,eC]=ident.split('|').map((n,i) => (i === 4?0: parseInt(n,10)))) => new Range(sl,sc,el,eC))();
+const rts = new Map<Range,string>();
+const str = new Map<string,Range>();
+
+export const rangeToIdentifier = (r:Range,pseudo?:Pseudo) => (!pseudo && rts.has(r)?rts.get(r)!:(({start,end} = r) => ((s=`${start.line}|${start.character}|${end.line}|${end.character}${pseudo?`|${pseudo}` as const:'' as const}` as const) => (!pseudo && rts.set(r,s) , s))())());
+
+export const identifierToRange = (ident:RangeIdentifier) => (str.has(ident)?str.get(ident)!:(([sl,sc,el,eC]=ident.split('|').map((n,i) => (i === 4?0: parseInt(n,10)))) => ((r = new Range(sl,sc,el,eC)) => (str.set(ident,r),r))())());
 
 export const debounce = <T extends (...args:any[])=>any>(callback:T, wait:number) => {
   let timeoutId:ReturnType<typeof setTimeout> |undefined;
